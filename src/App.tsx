d1338ba5b8990a4764cec822eb1f5efb54e0d721
dhruv-pandey <dhruv.pandey93@gmail.com>
2024-08-02 13:21:20 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sample file for hosting1
+          Sample file for hosting222
         </p>
         <a
           className="App-link"
