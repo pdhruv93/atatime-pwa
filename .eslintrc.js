84433c28152ecff8ab482b2e8ece2420448fea6a
dhruv-pandey <dhruv.pandey93@gmail.com>
2024-08-02 14:48:19 +0300

    Install eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+export default {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'prettier', // prettier
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: {
+            jsx: true,
+        },
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint', 'react', 'prettier'], // prettier
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-uses-react': 'off',
+    },
+    settings: {
+        'import/resolver': {
+            typescript: {},
+        },
+    },
+};
